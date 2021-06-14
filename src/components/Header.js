cd913d488dc51a3ccc1dea587b730d3271e6a259
ruslankhh <ruslankhh@gmail.com>
2021-06-14 20:10:08 +0300

    feat: добавить логотип

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,7 @@
 import * as React from 'react'
+import styled from 'styled-components'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
 
 import Container from './Container'
 import LinkList from './LinkList'
@@ -9,30 +12,69 @@ const headerStyles = {
   paddingTop: '1em',
 }
 
-const hiddenHeaderStyles = {
-  ...headerStyles,
+const navStyles = {
+  position: 'relative',
+  marginTop: '1em',
+}
+
+const hiddenNavStyles = {
+  ...navStyles,
   opacity: 0,
   touchAction: 'none',
   pointerEvents: 'none',
-  '-moz-user-select': 'none',
-  '-webkit-user-select': 'none',
+  MozUserSelect: 'none',
+  WebkitUserSelect: 'none',
+}
+
+const ImageWrapper = styled.div`
+  display: block;
+  position: absolute;
+  width: 6em;
+  height: 6em;
+  margin-top: 9.5em;
+  margin-left: -7em;
+  z-index: 1;
+
+  @media (max-width: 768px) {
+    display: inline-block;
+    float: left;
+    position: relative;
+    margin-top: -1.5em;
+    margin-left: -1em;
+  }
+`
+
+const imageStyles = {
+  width: '100%',
+  height: '100%',
+  maxWidth: '100%',
+  maxHeight: '100%',
 }
 
 // data
 const links = [
-  { to: '/', text: 'Home' },
   { text: 'Talks' },
   { text: 'Blog' },
-  { text: 'About' },
 ]
 
 // markup
 const Header = ({ isHome = false }) => {
   return (
-    <header style={isHome ? hiddenHeaderStyles : headerStyles}>
+    <header style={headerStyles}>
       <Container>
-        <div>Ruslan Khusnetdinov</div>
-        <nav>
+        <ImageWrapper>
+          <Link to={'/'}>
+            <StaticImage style={imageStyles}
+              alt="Illustration of the head of Ruslan Khusnetdinov"
+              src="../images/icon.png"
+              placeholder="none"
+              layout="fixed"
+              width={100}
+              height={100}
+            />
+          </Link>
+        </ImageWrapper>
+        <nav style={isHome ? hiddenNavStyles : navStyles}>
           <LinkList items={links} />
         </nav>
       </Container>
