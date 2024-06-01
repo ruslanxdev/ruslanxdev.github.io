478bdf9a348e508aebf716828c959df97ee9e647
ruslanxdev <ruslankhh@gmail.com>
2024-06-01 13:21:13 +0300

    feat: обновить типографику и cv

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,18 @@
 import * as React from 'react'
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from 'gatsby'
 import { createGlobalStyle } from 'styled-components'
-import { Link } from "gatsby"
-import { MDXProvider } from "@mdx-js/react"
+import { Link } from 'gatsby'
+import { MDXProvider } from '@mdx-js/react'
 
+import SubHeader from './SubHeader.js'
 import Container from './Container.js'
 import Header from './Header.js'
 import Footer from './Footer.js'
 
-const shortcodes = { Link }
+const shortcodes = {
+  SubHeader,
+  Link,
+}
 
 // styles
 const GlobalStyle = createGlobalStyle`
@@ -25,18 +29,55 @@ const GlobalStyle = createGlobalStyle`
   }
 
   main {
-    font-size: 1.25rem;
+    font-size: 1.125rem;
     line-height: 1.34;
   }
 
-  h1, h2, h3 {
+  h1, h2, h3, h4 {
+    margin-bottom: 0;
     line-height: 1;
   }
 
+  h1 span, h2 span, h3 span, h4 span {
+    font-weight: normal;
+    color: darkgray;
+    font-size: 0.777778em;
+  }
+
   h1 {
-    font-size: 3rem;
     margin-top: 3rem;
+    font-size: 3rem;
     margin-bottom: 1.5rem;
+  }
+
+  h2 {
+    margin-top: 2.25rem;
+    font-size: 2.25rem;
+  }
+
+  h3 {
+    margin-top: 1.5rem;
+    font-size: 1.75rem;
+  }
+
+  h4 {
+    margin-top: 1.25rem;
+    font-size: 1.375rem;
+  }
+
+  blockquote {
+    margin-left: 0;
+    margin-right: 0;
+    font-size: 1.25rem;
+  }
+
+  p {
+    margin-bottom: 0;
+  }
+
+  ul {
+    margin-top: 0.25rem;
+    padding-inline-start: 2rem;
   }
 `
 
