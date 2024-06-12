0fd5cbcae3e2e9744f9f9ff9d04d851938ea912b
ruslanxdev <ruslankhh@gmail.com>
2024-06-12 12:37:05 +0300

    fix: поправить иконку

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -51,25 +51,44 @@ const config = {
         icons: [
           {
             "src": "icons/icon-128.png",
-            "sizes": "32x32 48x48",
+            "sizes": "48x48",
             "type": "image/png"
           },
           {
             "src": "icons/icon-196.png",
-            "sizes": "72x72 96x96 128x128 196x196",
+            "sizes": "72x72",
             "type": "image/png"
           },
           {
             "src": "icons/icon-196.png",
-            "sizes": "72x72 96x96 128x128 196x196",
-            "type": "image/png",
-            "purpose": "maskable"
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-196.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-196.png",
+            "sizes": "192x192",
+            "type": "image/png"
           },
           {
             "src": "icons/icon-512.png",
             "sizes": "256x256",
             "type": "image/png"
           },
+          {
+            "src": "icons/icon-512.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
         ],
       },
     },
