97b70363928207496ac531ab8c9e783db60887e7
Ruslan X <ruslankhh@gmail.com>
2025-03-09 18:29:19 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const Page = () => {
       <blockquote>
         <p>
           Привет! Я продакт-менеджер и разработчик. Работаю в&nbsp;<s style={({color: '#ed6c74'})}>Uchi.ru</s> → <s style={({color: '#ffcc00'})}>Яндекс Поиске</s> → <b style={({color: '#4daaf8'})}>Яндекс Лавке</b> (<Link to='/cv'>резюме</Link>). 
-          Пишу в&nbsp;телеграм-канал <Link to='https://t.me/ruslanxdev'>Технопродакт</Link>.</p>
+          Пишу в&nbsp;телеграм-канал <Link to='https://t.me/ruslanxtech'>Технопродакт</Link>.</p>
         <p>До этого долгое время проективровал мосты.</p>
       </blockquote>
     </Layout>
